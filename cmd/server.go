18bd8494efa193877f152d659009fa9478e6e21a
Phonepadith PHOUMMAVONG <phonepadithpp@gmail.com>
2023-05-09 17:29:40 +0700

    update config

--- cmd/server.go
+++ cmd/server.go
@@ -21,12 +21,12 @@ var (
 	chainIDMap = map[string]int{"goerli": 5, "sepolia": 11155111}
 
 	httpPortFlag = flag.Int("httpport", 8080, "Listener port to serve HTTP connection")
-	proxyCntFlag = flag.Int("proxycount", 0, "Count of reverse proxies in front of the server")
+	proxyCntFlag = flag.Int("proxycount", 5, "Count of reverse proxies in front of the server")
 	queueCapFlag = flag.Int("queuecap", 100, "Maximum transactions waiting to be sent")
 	versionFlag  = flag.Bool("version", false, "Print version number")
 
 	payoutFlag   = flag.Int("faucet.amount", 1, "Number of Ethers to transfer per user request")
-	intervalFlag = flag.Int("faucet.minutes", 1440, "Number of minutes to wait between funding rounds")
+	intervalFlag = flag.Int("faucet.minutes", 30, "Number of minutes to wait between funding rounds")
 	netnameFlag  = flag.String("faucet.name", "testnet", "Network name to display on the frontend")
 
 	keyJSONFlag  = flag.String("wallet.keyjson", os.Getenv("KEYSTORE"), "Keystore file to fund user requests with")
